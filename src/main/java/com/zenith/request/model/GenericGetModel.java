0f06e6c47ae7185c1a6201d219ab77de0a3a5916
caleb <waynecounty15@gmail.com>
2017-12-25 23:25:15 -0500

    Flag post is tested and works, also updated endpoints sheet

---
+++ src/main/java/com/zenith/request/model/GenericGetModel.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zenith.request.model;
+
+/**
+ *
+ * @author wayne
+ */
+public class GenericGetModel {
+    
+    private String token; 
+
+    /**
+     * @return the token
+     */
+    public String getToken() {
+        return token;
+    }
+
+    /**
+     * @param token the token to set
+     */
+    public void setToken(String token) {
+        this.token = token;
+    }
+    
+}
