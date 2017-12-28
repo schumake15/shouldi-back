034e3775a6a94ed33a30aef0d2c6a2f8c5b45d3f
caleb <waynecounty15@gmail.com>
2017-12-27 22:47:12 -0500

    Get all messages feature is now implemented

--- src/main/java/com/zenith/entrypoints/MessageEntryPoint.java
+++ src/main/java/com/zenith/entrypoints/MessageEntryPoint.java
@@ -1,5 +1,7 @@
 package com.zenith.entrypoints;
 
+import com.zenith.Beans.MessageBean;
+import com.zenith.request.model.GenericGetModel;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -8,15 +10,33 @@ import javax.ws.rs.core.MediaType;
 
 import com.zenith.request.model.MessageModel;
 import com.zenith.service.MessageService;
+import com.zenith.templates.PostTemplate;
+import com.zenith.user.response.MessageWrapper;
+import java.util.List;
+import javax.ws.rs.POST;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.Response;
 
 @Path("/messages")
 public class MessageEntryPoint {
+
     @GET
     @Path("/send")
     @Consumes(MediaType.APPLICATION_JSON) // our method consumes or takes in json data
     @Produces({MediaType.APPLICATION_JSON})
     public void sendMessage(MessageModel message) {
-    	MessageService service = new MessageService();
-    	service.sendMessage(message);    	
+        MessageService service = new MessageService();
+        service.sendMessage(message);
     }
+
+    @POST
+    @Path("/my/messages")
+    @Consumes(MediaType.APPLICATION_JSON) // our method consumes or takes in json data
+    @Produces({MediaType.APPLICATION_JSON})
+    public MessageWrapper getMessages(GenericGetModel getModel){
+        MessageService service = new MessageService(); 
+        MessageWrapper wrapper = new MessageWrapper(service.getMyMessages(getModel)); 
+        return wrapper; 
+    }
+
 }
--- src/main/java/com/zenith/service/MessageService.java
+++ src/main/java/com/zenith/service/MessageService.java
@@ -1,12 +1,29 @@
 package com.zenith.service;
 
+import com.zenith.Beans.MessageBean;
 import com.zenith.DAO.MessageDao;
+import com.zenith.request.model.GenericGetModel;
 import com.zenith.request.model.MessageModel;
+import java.util.List;
 
 public class MessageService {
-	public void sendMessage(MessageModel message) {
-		MessageDao dao= new MessageDao();
-		dao.sendMessage(message);
-		
-	}
+
+    MessageDao dao = null; 
+    
+    public MessageService(){
+        dao = new MessageDao(); 
+    }
+    public void sendMessage(MessageModel message) {
+        dao.openConnection();
+        dao.sendMessage(message); 
+        dao.closeConnection();
+    }
+    
+    public List<String> getMyMessages(GenericGetModel getModel) {
+        
+        return dao.getMyMessages(getModel); 
+        
+    }
+    
+    
 }
